8e01a5228d3d1ada7689d2977e09c88258864769
Festus <savalifestus82@gmail.com>
2025-08-30 18:48:43 +0300

    feat(auth): implement user login, logout, and registration with templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register_view, CustomLoginView, CustomLogoutView
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
-    path('books/', list_books, name='book_list'),
-    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path("register/", register_view, name="register"),
-    path("login/", CustomLoginView.as_view(), name="login"),
-    path("logout/", CustomLogoutView.as_view(), name="logout"),
+    path("books/", views.list_books, name="book_list"),
+    path("libraries/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+    path("register/", views.register, name="register"),
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
 ]
