aff6bba0c9e00cef33df35b3396e70cb2f75c631
Festus <savalifestus82@gmail.com>
2025-08-30 17:51:13 +0300

    feat(relationship_app): add function-based and class-based views for books and libraries

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Library, Book
 
 def book_list(request):
     """Display all books and their authors"""
